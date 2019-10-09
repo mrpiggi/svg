70adcebd70291815f9e38ca6f8d289da4abfded1
mrpiggi <hanisch.latex@outlook.com>
2019-10-10 00:50:13 +0200

    Revert mistakenly commits to 'master' back to last release 26dd5bb5fd87be6ee71e603492f5394ab1d0347b

--- source/clearsource.bat
+++ source/clearsource.bat
@@ -3,10 +3,7 @@ attrib +h "Makefile*" /s
 attrib +h "*.bat" /s
 attrib +h "*.dtx" /s
 attrib +h "*.mak" /s
-attrib +h "examples\*.svg" /s
-attrib +h "examples\*.tex" /s
-attrib +h "examples\*.c" /s
-attrib +h "examples\*.S" /s
+attrib +h "examples\*.*" /s
 del "*.*" /s /q> nul
 if exist svg-inkscape rmdir svg-inkscape /s /q > nul
 if exist svg-extract rmdir svg-extract /s /q > nul
